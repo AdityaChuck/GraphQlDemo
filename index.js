91b342d8fb9380a5a7ba735476edaca2e445f8c0
AdityaChuck <adityanarayanmahaptra00@gmail.com>
2020-08-20 03:56:00 +0530

    20-08-2020-03:55AM

---
+++ index.js
@@ -0,0 +1,24 @@
+const { ApolloServer } = require('apollo-server')
+const gql = require('graphql-tag')
+const mongoose = require('mongoose')
+
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers/index')
+const { MONGODB } = require('./config')
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+mongoose.connect(MONGODB, {useUnifiedTopology: true, useNewUrlParser: true})
+.then(() => {
+    console.log(`MONGODB Connected`);
+    return server.listen({port: 5000})
+})
+.then(res => {
+    console.log(`server running at ${res.url}`);
+})
+
+    
