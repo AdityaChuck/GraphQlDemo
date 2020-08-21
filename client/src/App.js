259dc7f020e449b23e615c925be35ceb2e4338a5
AdityaChuck <adityanarayanmahaptra00@gmail.com>
2020-08-22 01:47:04 +0530

    22-08-2020 01:47 AM

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import { AuthProvider } from "./context/Auth";
+import AuthRoute from './util/AuthRoute'
+
+import MenuBar from "./components/MenuBar";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      Connectify
-    </div>
+    <Container>
+      <AuthProvider>
+        <Router>
+          <MenuBar />
+          <Switch>
+            <Route exact path="/" render={(props) => <Home {...props} />} />
+            <AuthRoute
+              exact
+              path="/login"
+              component={Login}
+            />
+            <AuthRoute
+              exact
+              path="/register"
+              component={Register}
+            />
+          </Switch>
+        </Router>
+      </AuthProvider>
+    </Container>
   );
 }
 
